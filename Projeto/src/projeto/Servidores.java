96603756126028b390af63fb3668f9dd8e328df1
a81317 <36645389+a81317@users.noreply.github.com>
2018-12-18 01:14:31 +0000

    Revert "preciso de ajuda"
    This reverts commit e9bb5b584eabe22d8a98c4009ae5b4336fe965a4.

--- Projeto/src/projeto/Servidores.java
+++ Projeto/src/projeto/Servidores.java
@@ -42,38 +42,26 @@ public class Servidores {
     public int reservaPedido(String nome) {
         
         l.lock();
-        Servidor s;
         try {
-                 
+            Servidor s;     
             while (this.vazios.isEmpty() && this.leilao.isEmpty()) {
                 c.await();
             }
             
             if (!this.vazios.isEmpty()) {
-                s = this.vazios.get(0);
-                this.vazios.remove(0);
+                this.vazios.get(0);
+            }
 
-            }
-            else { //Mudar para valor mais baixo
-                s = this.leilao.get(0);
-                this.leilao.remove(0);
-            }
             
             s.reserva(nome);
-            this.pedido.put(nome,s);
-           
-
-            
+            this.ocupados.put(nome,s);
+            this.vazios.remove(0);
+            return 1;
         }
         catch (InterruptedException ex) {
-            System.out.printf("Erro"); 
-            return -1;
-        }        
-        finally {
+            System.out.printf("Erro");
+        }        finally {
             l.unlock();
-            return s.getId();  
-            
         }
-         
     }
 }
